a9cf1baea1b173125837b1d7fef4a516def1d160
Monika Tantia <monika.tantia@klm.com>
2019-03-21 11:26:50 +0100

    log import updated

--- flogo-sandbox/activity/amt_add_sigTo_incoming_headers/activity.go
+++ flogo-sandbox/activity/amt_add_sigTo_incoming_headers/activity.go
@@ -27,7 +27,7 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
     http.HandleFunc("/", handler)
-    log.Error(http.ListenAndServe("localhost:8000", nil))
+  //  log.Error(http.ListenAndServe("localhost:8000", nil))
 	return true, nil
 }
 
