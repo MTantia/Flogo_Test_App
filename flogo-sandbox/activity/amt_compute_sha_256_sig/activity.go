6bf6d58e9432c2b57ec43721d1842f0a2624ca4e
Monika Tantia <monika.tantia@klm.com>
2019-03-27 14:24:39 +0100

    updating files

--- flogo-sandbox/activity/amt_compute_sha_256_sig/activity.go
+++ flogo-sandbox/activity/amt_compute_sha_256_sig/activity.go
@@ -1,38 +1,59 @@
 package amt_compute_sha_256_sig
 
 import (
-	
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"crypto/sha256"
-	"fmt"
-	"time"
 	"strconv"
-
+	"time"
 )
 
-var log = logger.GetLogger("AMT Compute SHA56 Sig")
+// log is the default package logger
+var log = logger.GetLogger("amt_compute_sha_256_sig")
 
-// MyActivity is a stub for your Activity implementation
-type MyActivity struct {
+const (
+	ivKey = "key"
+	ivSecret = "secret"
+
+	ovHex = "hex"
+	ovBase64 = "base64"
+)
+
+// Sha256Activity is a stub for your Activity implementation
+type Sha256Activity struct {
 	metadata *activity.Metadata
 }
 
 // NewActivity creates a new activity
 func NewActivity(metadata *activity.Metadata) activity.Activity {
-	return &MyActivity{metadata: metadata}
+	return &Sha256Activity{metadata: metadata}
 }
 
 // Metadata implements activity.Activity.Metadata
-func (a *MyActivity) Metadata() *activity.Metadata {
+func (a *Sha256Activity) Metadata() *activity.Metadata {
 	return a.metadata
 }
 
-func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+// Eval implements activity.Activity.Eval
+func (a *Sha256Activity) Eval(context activity.Context) (done bool, err error) {
 
-	sha256_Signature := sha256.New()
-	sha256_Signature.Write([]byte("pvz3r3qgafb6qcaapgjt68nj" + "vNubFQXk7r" +strconv.FormatInt(time.Now().Unix(),10) ))
-	fmt.Printf("%x", sha256_Signature.Sum(nil))
-	context.SetOutput("sha256_Signature", sha256_Signature.Sum(nil));
+	key := context.GetInput(ivKey).(string)
+	secret := context.GetInput(ivSecret).(string)
+
+	s := key + secret + strconv.FormatInt(time.Now().Unix(), 10)
+	hash := sha256.New()
+	hash.Write([]byte(s))
+
+	hex := hex.EncodeToString(hash.Sum(nil))
+	log.Debugf("hex = %s", hex)
+
+	base64 := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+	log.Debugf("base64 = %s", base64)
+
+	context.SetOutput(ovHex, hex)
+	context.SetOutput(ovBase64, base64)
+
 	return true, nil
 }
